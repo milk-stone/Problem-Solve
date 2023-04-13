568d8aa9edb046239e48a9f1a3ff2ea42216fa22
milk-stone <86397457+milk-stone@users.noreply.github.com>
2023-04-14 01:18:37 +0900

    [Gold I] Title: 조약돌, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/25378. 조약돌/조약돌.py
+++ 백준/Gold/25378. 조약돌/조약돌.py
@@ -4,26 +4,24 @@ input = sys.stdin.readline
 
 n = int(input())
 stones = list(map(int, input().split()))
-skip = [[False for _ in range(n)] for _ in range(n)]
-dp = [987654321 for _ in range(n)]
-dp[0] = 1
+skip = [[False for _ in range(n+1)] for _ in range(n+1)]
+dp = [0 for _ in range(n+1)]
+dp[1] = 1
 
 
-for start in range(n-1):
+for start in range(1, n):
     sign = 1
     temp = 0
-    for i in range(start, n):
-        temp += stones[i] * sign
+    for i in range(start, n+1):
+        temp += stones[i-1] * sign
         sign *= -1
         if temp == 0:
             skip[start][i] = True
 
-for i in range(n-1):
-    for j in range(i+1, n):
-        dp[j] = min(dp[j], dp[j-1] + 1)
-        if skip[i][j]:
-            if i == 0:
-                dp[j] = min(j-i, dp[j])
-                continue
-            dp[j] = min(j - i + dp[i-1], dp[j])
+
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + 1
+    for j in range(1, i):
+        if skip[j][i]:
+            dp[i] = min(i - j + dp[j-1], dp[i])
 print(dp[-1])
