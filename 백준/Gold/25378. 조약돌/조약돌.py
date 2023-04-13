3c20dc541bc3d10cd7c0afb29dce2386f290934e
milk-stone <86397457+milk-stone@users.noreply.github.com>
2023-04-14 00:34:56 +0900

    [Gold I] Title: 조약돌, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/25378. 조약돌/조약돌.py
+++ 백준/Gold/25378. 조약돌/조약돌.py
@@ -5,22 +5,7 @@ input = sys.stdin.readline
 n = int(input())
 stones = list(map(int, input().split()))
 skip = [[False for _ in range(n)] for _ in range(n)]
-dp = [[987654321 for _ in range(n)] for _ in range(n)]
-
-
-def solve(start, end):
-    if start == end:
-        dp[start][end] = 1
-        return dp[start][end]
-    if dp[start][end] != 987654321:
-        return dp[start][end]
-    if skip[start][end]:
-        dp[start][end] = end - start
-    for i in range(start, end):
-        left = solve(start, i)
-        right = solve(i+1, end)
-        dp[start][end] = min(left + right, dp[start][end])
-    return dp[start][end]
+dp = [i+1 for i in range(n)]
 
 
 for start in range(n-1):
@@ -32,4 +17,11 @@ for start in range(n-1):
         if temp == 0:
             skip[start][i] = True
 
-print(solve(0, n-1))
+for i in range(n-1):
+    for j in range(i+1, n):
+        if skip[i][j]:
+            if i == 0:
+                dp[j] = min(j-i, dp[j])
+            else:
+                dp[j] = min(j-i+dp[i-1], dp[j])
+print(dp[-1])
