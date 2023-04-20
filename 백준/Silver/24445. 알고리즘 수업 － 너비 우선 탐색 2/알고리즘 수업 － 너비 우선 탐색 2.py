5b6b579b524b1a2095e0fd09c8628e6029b0aa98
milk-stone <86397457+milk-stone@users.noreply.github.com>
2023-04-20 18:37:48 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 464 ms, Memory: 135084 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,27 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+N, M, start = map(int, input().split())
+visited = [0 for _ in range(N+1)]
+graph = [[] for _ in range(N+1)]
+for i in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+for i in range(1, N+1):
+    graph[i].sort(reverse=True)
+q = deque([start])
+count = 1
+visited[start] = 1
+count += 1
+while q:
+    now = q.popleft()
+    for next in graph[now]:
+        if visited[next] == 0:
+            q.append(next)
+            visited[next] = count
+            count += 1
+for i in range(1, N+1):
+    print(visited[i])
