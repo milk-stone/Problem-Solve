3d49933c2093c130ed1064a3d1559106f8a182e0
Wooseok Yu <86397457+milk-stone@users.noreply.github.com>
2025-01-26 21:52:50 +0900

    [Silver I] Title: 곱셈, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,20 @@
+import sys
+
+
+input = sys.stdin.readline
+
+a, b, c = map(int, input().split())
+
+def solve(a, b, c):
+    if b == 1:
+        return a % c
+
+    temp = solve(a, b//2, c)
+
+    if b % 2 == 0:
+        return (temp * temp) % c
+    else:
+        return (temp * temp * a) % c
+
+
+print(solve(a, b, c))
