03e87629445fcdcf2bec9166c0b030b3465dfdd7
Wooseok Yu <86397457+milk-stone@users.noreply.github.com>
2026-02-10 16:45:00 +0900

    [Gold V] Title: 센서, Time: 84 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.py
+++ 백준/Gold/2212. 센서/센서.py
@@ -7,31 +7,9 @@ K = int(input())
 arr = list(map(int, input().split()))
 arr.sort()
 
-interval = []
+dist = []
+for i in range(1, N):
+    dist.append(arr[i] - arr[i - 1])
 
-for i in range(N):
-    interval.append([arr[i], arr[i]])
-
-ans = 0
-while len(interval) > K:
-    minCount = 1_000_001
-    left, right = -1, -1
-    for i in range(1, len(interval)):
-        v = interval[i][0] - interval[i - 1][1]
-        if v < minCount:
-            minCount = v
-            left, right = i - 1, i
-
-    ans += minCount
-
-    new_interval = []
-    for i in range(len(interval)):
-        if i == left:
-            new_interval.append([interval[left][0], interval[right][1]])
-        elif i == right:
-            continue
-        else:
-            new_interval.append(interval[i])
-    interval = new_interval
-
-print(ans)
+dist.sort(reverse=True)
+print(sum(dist[K - 1:]))
