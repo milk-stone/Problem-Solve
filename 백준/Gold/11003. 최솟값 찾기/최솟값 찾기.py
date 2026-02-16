00cc91f6d9efa2a6664a575c70e4419035479438
Wooseok Yu <86397457+milk-stone@users.noreply.github.com>
2026-02-16 19:09:10 +0900

    [Gold I] Title: 최솟값 찾기, Time: 2624 ms, Memory: 710764 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
@@ -1,41 +1,21 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
 N, L = map(int, input().split())
 arr = list(map(int, input().split()))
 
-power_of_two = 1
-while power_of_two <= N:
-    power_of_two *= 2
-
-tree = [1_000_000_001] * (2 * power_of_two)
+q = deque()
+ans = []
 for i in range(N):
-    tree[power_of_two + i] = arr[i]
+    curValue = arr[i]
+    while q and q[-1][1] > curValue:
+        q.pop()
 
-for i in range(2 * power_of_two - 2, 0, -2):
-    tree[i // 2] = min(tree[i], tree[i + 1])
+    q.append((i, curValue))
+    if q[0][0] <= i - L:
+        q.popleft()
 
-
-def search(left, right):
-    l = left + power_of_two
-    r = right + power_of_two
-    ans = 1_000_000_001
-    while l <= r:
-        if l % 2 == 1:
-            ans = min(ans, tree[l])
-            l += 1
-        if r % 2 == 0:
-            ans = min(ans, tree[r])
-            r -= 1
-        l //= 2
-        r //= 2
-    return ans
-
-answer = []
-for i in range(N):
-    if i - (L - 1) < 0:
-        answer.append(search(0, i))
-    else:
-        answer.append(search(i - L + 1, i))
-print(*answer)
+    ans.append(q[0][1])
+print(*ans)
