c370eaec3f68580caba0c58cac094927afab97da
Wooseok Yu <86397457+milk-stone@users.noreply.github.com>
2026-03-09 18:12:31 +0900

    [Gold II] Title: 벡터 매칭, Time: 584 ms, Memory: 115820 KB -BaekjoonHub

--- 백준/Gold/1007. 벡터 매칭/벡터 매칭.py
+++ 백준/Gold/1007. 벡터 매칭/벡터 매칭.py
@@ -3,15 +3,15 @@ import sys
 input = sys.stdin.readline
 
 minDistance = float('inf')
-def cal(vector, curIndex, dots, plusCount, minusCount, N):
+def cal(vector_x, vector_y, curIndex, dots, plusCount, minusCount, N):
     # print(f"[DEBUG] {vector}, {curIndex}, {dots}, {plusCount, minusCount, N}")
     global minDistance
     if plusCount < N // 2:
-        cal([vector[0] + dots[curIndex + 1][0], vector[1] + dots[curIndex + 1][1]], curIndex + 1, dots, plusCount + 1, minusCount, N)
+        cal(vector_x + dots[curIndex + 1][0], vector_y + dots[curIndex + 1][1], curIndex + 1, dots, plusCount + 1, minusCount, N)
     if minusCount < N // 2:
-        cal([vector[0] - dots[curIndex + 1][0], vector[1] - dots[curIndex + 1][1]], curIndex + 1, dots, plusCount, minusCount + 1, N)
+        cal(vector_x - dots[curIndex + 1][0], vector_y - dots[curIndex + 1][1], curIndex + 1, dots, plusCount, minusCount + 1, N)
     if plusCount + minusCount == N:
-        curDistance = (vector[0] ** 2 + vector[1] ** 2) ** (0.5)
+        curDistance = (vector_x ** 2 + vector_y ** 2) ** (0.5)
         if curDistance < minDistance:
             minDistance = curDistance
 
@@ -23,8 +23,7 @@ for _ in range(T):
     for __ in range(N):
         dots.append(list(map(int, input().split())))
 
-    cal(dots[0], 0, dots, 1, 0, N)
-    cal([-dots[0][0], -dots[0][1]], 0, dots, 0, 1, N)
+    cal(dots[0][0], dots[0][1], 0, dots, 1, 0, N)
 
     print(minDistance)
 
